7a9f0b0296862064432bdcf6097ae6754cb61f94
Md Musa <iammdmusa@users.noreply.github.com>
2018-02-05 21:18:28 +0100

    add cross

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ app.use(session({
   })
 }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 // parse incoming requests
